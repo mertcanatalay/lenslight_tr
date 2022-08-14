71ce16b845ae8e5269749f9cc164a3cbe295cc5d
Mert Can Atalay <mert.atalay@dincerlojistik.com>
2022-08-14 22:01:52 +0300

    lesson 01

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res) => {
+    res.send("index sayfası 2");
+});
+
+app.listen(port,()=>{
+    console.log(`application runing on port: ${port}`)
+});
