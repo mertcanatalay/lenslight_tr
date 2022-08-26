875d4f6c19176b647bcc047bd8fdc8d004ab8b8a
Mert Can Atalay <mert.atalay@dincerlojistik.com>
2022-08-26 09:41:58 +0300

    second 6

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
+
+dotenv.config();
+
+//connection to the db
+conn();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.get("/", (req,res) => {
-    res.send("index sayfası 2");
-});
+//ejs template engine 
+app.set("view engine", "ejs")
+
+//static files middlaware
+app.use(express.static('public'));
+app.use(express.json());
+
+//routes
+app.use("/", pageRoute)
+app.use("/photos", photoRoute);
 
 app.listen(port,()=>{
     console.log(`application runing on port: ${port}`)
